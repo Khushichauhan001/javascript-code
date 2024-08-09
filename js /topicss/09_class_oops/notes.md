c09a2cd0f93bf08510ca18b5757026f6a962e62d
Khushichauhan <csds23176@glbitm.ac.in>
2024-08-09 22:00:53 +0530

    oops

---
+++ js /topicss/09_class_oops/notes.md
@@ -0,0 +1,24 @@
+# javascript and classes
+
+## OOP (it is a process of writing , it has syntax )
+
+## object 
+ - collection of properties and methods
+ - toLowerCase , promises , etc are also objects
+
+ ## why use oops
+
+ ## parts of oop
+   object literal <!--in oop.js in detail -->
+
+ - constructor function
+ - prototypes
+ -classes
+ - instances (new , this)
+
+ ## 4 pillars
+ abstraction  :- hides internal detail 
+ encapsulation :- basically wrap up the function
+ inheritance :- 
+ polymorphism :- single method of doing many works
+
---
+++ js /topicss/09_class_oops/oop.js
@@ -0,0 +1,41 @@
+// object literal
+const user = {
+    username: "khushi",
+    loginCOunt: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        // console.log("got user details from databaase");
+        // console.log(`Username: ${this.username}`)  // jab bht sare users ho or hm kisi ek ka data chahiye toh use this keyword
+        console.log(this)  // it contains the data of the object
+    }
+}
+console.log(user.username);
+console.log(user.getUserDetails());
+// console.log(this)   // in global scope , it contains the empty object bcz it works as a window global object and it has many many more things in it 
+
+
+// constructor function 
+// const promiseOne = new Promise()  // new keyword is an constructor fnc which allows u ki aap ek hi object se multiple instances bna sko
+// const date = new Date()
+
+function User(username , isLoggedIn , loginCOunt){
+    // username = username ... isme ghme ni pta konsa variable h konsa passed parameter h 
+    this.username = username;  // this.username is variable
+    this.isLoggedIn = isLoggedIn;
+    this.loginCOunt = loginCOunt
+
+    return this  // no need 
+ }
+ const userOne = new User("khushi", 12 , true) // new dene s apni apni value print hogi brna jse niche likh h console bale comment to wo vakue overwrite hojangi
+ const userTwo = new User("chauhan", 11 , false)
+ console.log(userOne);  // it overwrite the userTwo values in userOne so its not good method
+
+ // what happens when we use new keyword
+ // step 1 : an empty object is made
+ // step 2 : a constructor function is call
+ // step 3 : all instances are injected into this keyword
+ // stp 4 : we get function
+
+ // read about instanceof function in js
+ 
