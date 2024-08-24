7f1ee8f75ccaff6bd0437c457b94399d756fb6ba
Your Name~ <csds23176@glbitm.ac.in>
2024-08-24 23:49:12 +0530

    flip

--- js /topicss/08_advance/fetch.js
+++ js /topicss/08_advance/fetch.js
@@ -17,7 +17,7 @@
 
 // interview ask
 // A fetch() promise only rejects when the request fails, for example, because of a badly-formed request URL or a network error. A fetch() promise does not reject if the server responds with HTTP status codes that indicate errors (404, 504, etc.).    
-fetch('https://api.github.com/users/Khushichauhan001')
+fetch('https://api.github.com/users/Khushichauhan001')            // it gives error but run it on console so it will gie output
 .then((response) => {
     return response.json()
 })
